61b175e3e34d9975b9d4525468dd830f896929e0
chendingfeng <chendingfengmail@163.com>
2020-09-22 17:35:35 +0800

    增加登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
     <el-card class="login-form-layout">
-      <el-form>
+      <el-form :model="loginForm"
+               :rules="loginRules"
+               ref="loginForm"
+                >
         <h2 class="login-title">totoro-web</h2>
-        <el-form-item prop="">
-            <el-input name=""
+        <el-form-item prop="username">
+            <el-input name="username"
                       type="text"
+                      v-model="loginForm.username"
                       placeholder="请输入用户名">
               <span slot="prefix">
                 <svg-icon icon-class="user"></svg-icon>
               </span>
             </el-input>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input name="password"
+                    :type="pwdType"
+                    v-model="loginForm.password"
+                    placeholder="请输入密码">
+            <span slot="prefix">
+              <svg-icon icon-class="password"></svg-icon>
+            </span>
+            <span slot="suffix" @click="showPwd">
+              <svg-icon icon-class="eye"></svg-icon>
+            </span>
+          </el-input>
+        </el-form-item>
+        <el-form-item style="margin-bottom: 20px;text-align: center">
+          <el-button style="width: 45%" type="primary" :loading="loading" @click.native.prevent="handleLogin">
+            登录
+          </el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -18,15 +40,72 @@
 </template>
 
 <script>
+  import {isvalidUsername} from '@/utils/validate';
+  import {setSupport,getSupport,setCookie,getCookie} from '@/utils/support';
+
   export default {
-    name: '',
+    name: 'login',
     data(){
-        return {}
+      const validateUsername = (rule, value, callback) => {
+        console.log(rule, value);
+        if (!isvalidUsername(value)) {
+          callback(new Error('请输入正确的用户名'))
+        } else {
+          callback()
+        }
+      };
+      const validatePass = (rule, value, callback) => {
+        if (value.length < 3) {
+          callback(new Error('密码不能小于3位'))
+        } else {
+          callback()
+        }
+      };
+      return {
+        loginForm: {
+          username: '',
+          password: '',
+        },
+        loginRules: {
+          username: [{required: true, trigger: 'blur', validator: validateUsername}],
+          password: [{required: true, trigger: 'blur', validator: validatePass}]
+        },
+        pwdType: 'password',
+        loading: false,
+      }
     },
     components: {},
-    created(){},
-    mounted(){},
-    methods: {}
+    created(){
+    },
+    mounted(){
+    },
+    methods: {
+      showPwd() {
+        if (this.pwdType === 'password') {
+          this.pwdType = ''
+        } else {
+          this.pwdType = 'password'
+        }
+      },
+      handleLogin() {
+        this.$refs.loginForm.validate(valid => {
+          if (valid) {
+            this.loading = true;
+            this.$store.dispatch('Login', this.loginForm).then(() => {
+              this.loading = false;
+              setCookie("username",this.loginForm.username,15);
+              setCookie("password",this.loginForm.password,15);
+              this.$router.push({path: '/'})
+            }).catch(() => {
+              this.loading = false
+            })
+          } else {
+            console.log('参数验证不合法！');
+            return false
+          }
+        })
+      },
+    }
   }
 </script>
 <style scoped>
